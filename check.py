48dc36c05692d089d1baae0137771f7c31f119f2
Шатуева Полина Николаевна <p.shatueva@main.nsu.ru>
2024-11-13 14:13:24 +0700

    oki

--- check.py
+++ check.py
@@ -1,10 +1,28 @@
-import Libsort # наш модуль
-import time # модуль для замера времени
-Massiv = [random.randint(1,10) for i in range(N)]
-Start = time.time()
-Libsort.insertion_sort(Massiv)
-Finish = time.time()
-Res_msec = (Finish – Start)*1000
+import alg1  # наш модуль
+import time  # модуль для замера времени
 
-print(len(Massiv))
-print()
+N =10
+from random import randint
+
+Massiv = [randint(1, 1000) for i in range(N)]
+M =Massiv[:]
+Start = time.time()
+alg1.bubble_sort(M)
+Finish = time.time()
+Res_msec = (Finish - Start) * 1000
+print (Res_msec)
+Start = time.time()
+alg1.selection_sort(M)
+Finish = time.time()
+Res_msec = (Finish - Start) * 1000
+print (Res_msec)
+Start = time.time()
+alg1.insertion_sort(M)
+Finish = time.time()
+Res_msec = (Finish - Start) * 1000
+print (Res_msec)
+Start = time.time()
+alg1.selection_sort(M)
+Finish = time.time()
+Res_msec = (Finish - Start) * 1000
+print (Res_msec)
